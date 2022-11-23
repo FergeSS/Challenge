0968b19e7d14e9004878327eace51043aa0fb90c
FergeSS <97806343+FergeSS@users.noreply.github.com>
2022-11-23 22:10:41 +0300

    Create min.cpp

---
+++ min.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+bool c(const char *s,int c){while(*s&&c>=0)*s++==40?++c:--c;return !c;}
+
+bool testCase(const char str[], bool expected, bool& res) {
+    res = c(str, 0);
+    return res == expected;
+}
+
+int main()
+{
+    const char* str_cases[] = {
+        "",
+        "(",
+        ")",
+        "()",
+        ")(",
+        "()()",
+        "(()",
+        "())",
+        "(())",
+        ")(()))",
+        "(())((()())())"
+    };
+    const size_t cNumCases = sizeof(str_cases) / sizeof(char*);
+    bool expected[cNumCases] = { true, false, false, true, false, true, false, false, true, false, true };
+    bool result = true;
+    for (size_t iter = 0; iter < cNumCases; ++iter)
+    {
+        bool curr_check;
+        bool curr_test = testCase(str_cases[iter], expected[iter], curr_check);
+        std::cout << "test \"" << str_cases[iter] << "\" is " << (curr_test ? "ok" : "failed") << '\n';
+        std::cout << "\tresult: " << curr_check << " expected " << expected[iter] << '\n';
+        result = result && curr_test;
+    }
+    std::cout << "Total result: " << result << '\n';
+
+    return 0;
+}
